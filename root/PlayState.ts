2e293c37b99116664fdfafb3de832933e9d159fe
Johannes Ahlmann <johannes.ahlmann@emc.com>
2015-09-07 13:26:49 +0100

    moved player def, added "sprites" collide

--- root/PlayState.ts
+++ root/PlayState.ts
@@ -2,22 +2,19 @@
 /// <reference path="Main.ts" />
 /// <reference path="InitState.ts" />
 
-// need type annotation for autocomplete
-var player : Phaser.Sprite;
-
 
 /* preload game assets before starting the game */
 function preload() {
     game.load.image("player", "bunny.png");
-    
 }
 
 /* create the game world and entities */
 function create() {
-    player = sprites.create(450, 50, 'player');
+    var player = sprites.create(250, 50, 'player');
 }
 
 /* update step before each frame rendering */
 function update() {
+    game.physics.arcade.collide(sprites, sprites);
 }
 
--- root/js/game.js
+++ root/js/game.js
@@ -96,18 +96,17 @@ var InitState = (function (_super) {
 /// <reference path="lib/phaser.d.ts" />
 /// <reference path="Main.ts" />
 /// <reference path="InitState.ts" />
-// need type annotation for autocomplete
-var player;
 /* preload game assets before starting the game */
 function preload() {
     game.load.image("player", "bunny.png");
 }
 /* create the game world and entities */
 function create() {
-    player = sprites.create(450, 50, 'player');
+    var player = sprites.create(250, 50, 'player');
 }
 /* update step before each frame rendering */
 function update() {
+    game.physics.arcade.collide(sprites, sprites);
 }
 /// <reference path="lib/phaser.d.ts" />
 /// <reference path="PlayState.ts" />
